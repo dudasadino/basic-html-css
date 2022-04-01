67084b76ae7769f1707942250ecdfcff13ef68f1
duda sadino <dudababy27@gmail.com>
2022-04-01 10:40:25 -0300

    exercicio front html,css, javascript concluido

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', event => {
-    console.log('Agora posso ver o HTML')
-
     const requestFriendshipEl = document.querySelector("#request-friendship")
 
     requestFriendshipEl.addEventListener('click', event => {
@@ -8,43 +6,53 @@ document.addEventListener('DOMContentLoaded', event => {
 
         if (clickedElement.dataset.requestedFriendship === 'true') {
             clickedElement.innerText = "Solicitar amizade"
-
             clickedElement.classList.remove('button--active')
-            /* clickedElement.style.backgroundColor = "#3f51b5"
-            clickedElement.style.borderColor = "#29377e" */
-
             clickedElement.dataset.requestedFriendship = false
         } else {
             clickedElement.innerText = "Amizade solicitada"
-
             clickedElement.classList.add('button--active')
-            /* clickedElement.style.backgroundColor = "green"
-            clickedElement.style.borderColor = "darkGreen" */
-
             clickedElement.dataset.requestedFriendship = true
         }
     })
-
     document.querySelectorAll(".post-save").forEach(save =>
         save.addEventListener('click', event => {
             const clickSave = event.currentTarget
 
             if (clickSave.dataset.save === 'false') {
-                clickSave.innerText = 'Salvo'
+                clickSave.querySelector('span').innerText = 'Salvo'
+                clickSave.querySelector('svg').style.fill = 'red'
                 clickSave.dataset.save = true
             }
             else {
-                clickSave.innerText = 'Salvar'
+                clickSave.querySelector('span').innerText = 'Salvar'
+                clickSave.querySelector('svg').style.fill = 'none'
                 clickSave.dataset.save = false
             }
+
             const saveCount = Array.from(document.querySelectorAll(".post-save")).filter(post => post.innerText === 'Salvo').length
             const count = document.querySelector('#count')
             count.innerText = saveCount
         }))
 
-    const sendMessageEl = document.querySelector("#send-message")
-    sendMessageEl.addEventListener("click", function (event) {
+    const writeMessageEl = document.querySelector("#write-message")
+    writeMessageEl.addEventListener("click", function () {
         const message = document.querySelector("#message")
         message.style.display = "block"
     })
+
+    const sendMessageEl = document.querySelector("#send-message")
+    sendMessageEl.addEventListener("click", function (event) {
+        const texto = document.querySelector("#text")
+        if (texto.value == '') {
+            alert(`escreve a mensagem né!`)
+        }
+        else {
+            alert(`${texto.value} Tua mensagem foi enviada`)
+            const writeMessageEl = document.querySelector("#message")
+            writeMessageEl.style.display = 'none'
+            texto.value = ''
+        }
+    })
+
 })
+
